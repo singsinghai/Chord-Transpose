73367002a72da64df2083803251eb4b721567c80
singsinghai <duong.namhai123@gmail.com>
2022-02-18 16:30:22 +0700

    finish deploy app

--- deploy.py
+++ deploy.py
@@ -1,9 +1,6 @@
+import streamlit as st
 import my_functions
 from my_functions import *
-import streamlit as st
-
-def change_text(song):
-	pass
 
 def main():
 	st.title('Transpose streamlit app')
@@ -13,18 +10,14 @@ def main():
 		transpose_down = st.button('b')
 		transpose_up = st.button('#')
 
-
 	myform = st.form(key='text')
 	with myform:
 		text = st.text_area('Lyrics + chords, chords should be in [ ]')
 		submit = st.form_submit_button(label='Submit')
 
-
-	if 'song' not in st.session_state:
-		st.session_state.song = text
-
 	if submit:
-		st.write(text)
+		st.session_state.song = text
+		st.write(st.session_state.song)
 
 	if transpose_down:
 		st.session_state.song = transpose_song(st.session_state.song, -1)
